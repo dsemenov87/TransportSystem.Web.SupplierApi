d4b6dbd444f69c7b56f4356735c8984c5e82a845
dsemenov87 <dsemenov87@gmail.com>
2015-09-12 21:03:34 +0600

    create project

---
+++ TransportSystem.Web.SupplierApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransportSystem.Web.SupplierApi
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
